b78a57c33a532cc58880d765f5e57044fe780aed
Rafael <rafael_breno@outlook.com>
2022-08-13 20:03:50 -0300

    feat: Abstract Payout model

--- src/lib.rs
+++ src/lib.rs
@@ -5,66 +5,10 @@ pub mod models {
     pub mod leader;
     pub mod leaderboard;
     pub mod game;
-
-    // Payout
-    pub struct PayoutDetail {
-        pub user_id: String,
-        pub created_at: DateTime<Utc>,
-        pub payee_handle: String,
-        pub payer_handle: String,
-        pub payment_amount: f64,
-    }
-
-    pub fn new_payout_detail(
-        user_id: String,
-        created_at: DateTime<Utc>,
-        payee_handle: String,
-        payer_handle: String,
-        payment_amount: f64,
-        ) -> PayoutDetail {
-        PayoutDetail {
-            user_id,
-            created_at,
-            payee_handle,
-            payer_handle,
-            payment_amount,
-        }
-    }
-    
-    pub struct PayoutEvent {
-        pub event_id: String,
-        pub details: PayoutDetail,
-    }
-
-    pub fn new_payout_event (
-        event_id: String,
-        details: PayoutDetail,
-    ) -> PayoutEvent {
-        PayoutEvent {
-            event_id,
-            details,
-        }
-    }
-
-    pub struct Payout {
-        pub starting_after: String,
-        pub ending_before: String,
-        pub events: Option<[PayoutEvent; 0]>,
-    }
-
-    pub fn new_payout (
-        starting_after: String,
-        ending_before: String,
-        events: Option<[PayoutEvent; 0]>,
-    ) -> Payout {
-        Payout {
-            starting_after,
-            ending_before,
-            events,
-        }
-    }
+    pub mod payout;
 
     // Play
+    use leaderboard::*;
     pub struct Play {
         pub id: String,
         pub game_id: String,
