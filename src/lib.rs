b3399ce0653182afb5a861a8f2c63b5c5a59e8bf
Rafael <rafael_breno@outlook.com>
2022-08-13 00:18:26 -0300

    feat: Add Game model

--- src/lib.rs
+++ src/lib.rs
@@ -69,4 +69,40 @@ pub mod models {
             formatted_name,
         }
     }
+
+    pub struct Game {
+        pub id: String,
+        pub name: String,
+        pub description: String,
+        pub category: String,
+        pub developer_info: Option<String>,
+        pub tag_line: Option<String>,
+        pub url: String,
+        pub leaderboards: Option<[Leaderboard; 0]>, 
+        pub player_id: String,
+    }
+
+    pub fn new_game(
+            id: String,
+            name: String,
+            description: String,
+            category: String,
+            developer_info: Option<String>,
+            tag_line: Option<String>,
+            url: String,
+            leaderboards: Option<[Leaderboard; 0]>, 
+            player_id: String,
+        ) -> Game {
+        Game {
+            id,
+            name,
+            description,
+            category,
+            developer_info,
+            tag_line,
+            url,
+            leaderboards,
+            player_id,
+        }
+    }
 }
