400b01dc9b4d6051ca0912b7619398867429d2a2
Rafael <rafael_breno@outlook.com>
2022-08-13 19:52:59 -0300

    feat: Abstract Arcade model

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,6 @@
 pub mod models {
     use chrono::{DateTime, Utc};
-
-    // Arcade
-    #[derive(PartialEq,Debug)]
-    pub struct Arcade {
-        pub id: String,
-        pub name: String,
-        pub description: String,
-    }
-
-    pub fn new_arcade(id: String) -> Arcade {
-       Arcade {
-           id, 
-           name: String::from(""), 
-           description: String::from(""),
-       } 
-    }
+    pub mod arcade;
 
     // HasteEnvironment
     pub enum HasteEnvironment {
---
+++ src/models/arcade.rs
@@ -0,0 +1,14 @@
+#[derive(PartialEq,Debug)]
+pub struct Arcade {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+}
+
+pub fn new_arcade(id: String) -> Arcade {
+    Arcade {
+        id, 
+        name: String::from(""), 
+        description: String::from(""),
+    } 
+}
