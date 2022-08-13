a8d1ef2eefbf49eedc805b65db4ac5b8a6560da7
Rafael <rafael_breno@outlook.com>
2022-08-13 00:42:22 -0300

    feat: Add Payout Models

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod models {
+    use chrono::{DateTime, Utc};
+
     #[derive(PartialEq,Debug)]
     pub struct Arcade {
         pub id: String,
@@ -103,6 +105,63 @@ pub mod models {
             url,
             leaderboards,
             player_id,
+        }
+    }
+
+    pub struct PayoutDetail {
+        pub user_id: String,
+        pub created_at: DateTime<Utc>,
+        pub payee_handle: String,
+        pub payer_handle: String,
+        pub payment_amount: f64,
+    }
+
+    pub fn new_payout_detail(
+        user_id: String,
+        created_at: DateTime<Utc>,
+        payee_handle: String,
+        payer_handle: String,
+        payment_amount: f64,
+        ) -> PayoutDetail {
+        PayoutDetail {
+            user_id,
+            created_at,
+            payee_handle,
+            payer_handle,
+            payment_amount,
+        }
+    }
+    
+    pub struct PayoutEvent {
+        pub event_id: String,
+        pub details: PayoutDetail,
+    }
+
+    pub fn new_payout_event (
+        event_id: String,
+        details: PayoutDetail,
+    ) -> PayoutEvent {
+        PayoutEvent {
+            event_id,
+            details,
+        }
+    }
+
+    pub struct Payout {
+        pub starting_after: String,
+        pub ending_before: String,
+        pub events: Option<[PayoutEvent; 0]>,
+    }
+
+    pub fn new_payout (
+        starting_after: String,
+        ending_before: String,
+        events: Option<[PayoutEvent; 0]>,
+    ) -> Payout {
+        Payout {
+            starting_after,
+            ending_before,
+            events,
         }
     }
 }
