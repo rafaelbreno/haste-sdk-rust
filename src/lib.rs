a68af1c2f9e55ce7eb0044417c67cf8e0d326bb6
Rafael <rafael_breno@outlook.com>
2022-08-12 21:33:40 -0300

    feat: Add Leaderboard model

--- src/lib.rs
+++ src/lib.rs
@@ -26,12 +26,47 @@ pub mod models {
         pub avatar: Option<String>,
     }
 
-    pub fn new_leader(player_id: String, score: u64, name: Option<String>, avatar: Option<String>) -> Leader {
+    pub fn new_leader(
+            player_id: String,
+            score: u64,
+            name: Option<String>, 
+            avatar: Option<String>,
+        ) -> Leader {
         Leader {
             player_id,
             score,
             name,
             avatar,
+        }
+    }
+
+    pub struct Leaderboard {
+        pub id: String,
+        pub name: String,
+        pub cost: u64,
+        pub formatted_cost_string: String,
+        pub currency: String,
+        pub leaders: Option<[String; 0]>,
+        pub formatted_name: String,
+    }
+
+    pub fn new_leaderboard(
+            id: String,
+            name: String,
+            cost: u64,
+            formatted_cost_string: String,
+            currency: String,
+            leaders: Option<[String; 0]>, 
+            formatted_name: String,
+        ) -> Leaderboard {
+        Leaderboard {
+            id,
+            name,
+            cost,
+            formatted_cost_string,
+            currency,
+            leaders,
+            formatted_name,
         }
     }
 }
