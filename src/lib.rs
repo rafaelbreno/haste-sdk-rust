45419447530b28c05427ac31de7566ab9d05dbd9
Rafael <rafael_breno@outlook.com>
2022-08-12 21:26:31 -0300

    feat: Add Leader model

--- src/lib.rs
+++ src/lib.rs
@@ -18,4 +18,20 @@ pub mod models {
         Production,
         NonProduction,
     }
+
+    pub struct Leader {
+        pub player_id: String,
+        pub score: u64,
+        pub name: Option<String>,
+        pub avatar: Option<String>,
+    }
+
+    pub fn new_leader(player_id: String, score: u64, name: Option<String>, avatar: Option<String>) -> Leader {
+        Leader {
+            player_id,
+            score,
+            name,
+            avatar,
+        }
+    }
 }
