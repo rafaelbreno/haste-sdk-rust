087044b2fd4b29854c39a67bcfb1aad59def1e45
Rafael <rafael_breno@outlook.com>
2022-08-13 20:01:58 -0300

    feat: Abstract Game model

--- src/lib.rs
+++ src/lib.rs
@@ -4,45 +4,7 @@ pub mod models {
     pub mod haste_environment;
     pub mod leader;
     pub mod leaderboard;
-
-    use leaderboard::Leaderboard;
-
-    // Game
-    pub struct Game {
-        pub id: String,
-        pub name: String,
-        pub description: String,
-        pub category: String,
-        pub developer_info: Option<String>,
-        pub tag_line: Option<String>,
-        pub url: String,
-        pub leaderboards: Option<[Leaderboard; 0]>, 
-        pub player_id: String,
-    }
-
-    pub fn new_game(
-            id: String,
-            name: String,
-            description: String,
-            category: String,
-            developer_info: Option<String>,
-            tag_line: Option<String>,
-            url: String,
-            leaderboards: Option<[Leaderboard; 0]>, 
-            player_id: String,
-        ) -> Game {
-        Game {
-            id,
-            name,
-            description,
-            category,
-            developer_info,
-            tag_line,
-            url,
-            leaderboards,
-            player_id,
-        }
-    }
+    pub mod game;
 
     // Payout
     pub struct PayoutDetail {
