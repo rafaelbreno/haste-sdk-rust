c391a4281988656b2c8e167e2b4df75213855845
Rafael <rafael_breno@outlook.com>
2022-08-13 01:46:48 -0300

    feat: Add TopScore model

--- src/lib.rs
+++ src/lib.rs
@@ -343,4 +343,20 @@ pub mod models {
             game_id,
         }
     }
+
+    // TopScore
+    pub struct TopScore {
+        pub user_id: String,
+        pub value: u64,
+    }
+
+    pub fn new_top_score(
+        user_id: String,
+        value: u64,
+    ) -> TopScore {
+        TopScore {
+            user_id,
+            value,
+        }
+    }
 }
