846947cd868e83182b01dc1608daece15af551f1
Rafael <rafael_breno@outlook.com>
2022-08-12 21:19:05 -0300

    feat: Add Arcade model

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod models {
+    #[derive(PartialEq,Debug)]
+    pub struct Arcade {
+        pub id: String,
+        pub name: String,
+        pub description: String,
+    }
+
+    pub fn new_arcade(id: String) -> Arcade {
+       Arcade {
+           id, 
+           name: String::from(""), 
+           description: String::from(""),
+       } 
+    }
+}
---
+++ tests/models_arcade_test.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod models_test {
+    #[test]
+    fn test_new_arcade() {
+        let result = haste::models::new_arcade("some_id".to_string());
+        let expected = haste::models::Arcade{id:"some_id".to_string(), name: "".to_string(), description: "".to_string()};
+        assert_eq!(result, expected);
+    }
+}
