7f901c9ab236e84351fd7990a35d5467096903e6
Rafael <rafael_breno@outlook.com>
2022-08-13 20:11:31 -0300

    feat: Abstract models module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,1 @@
-pub mod models {
-    pub mod arcade;
-    pub mod haste_environment;
-    pub mod leader;
-    pub mod leaderboard;
-    pub mod game;
-    pub mod payout;
-    pub mod play;
-    pub mod player;
-    pub mod score;
-    pub mod token;
-    pub mod top_score;
-}
+pub mod models;
---
+++ src/models/mod.rs
@@ -0,0 +1,11 @@
+pub mod arcade;
+pub mod haste_environment;
+pub mod leader;
+pub mod leaderboard;
+pub mod game;
+pub mod payout;
+pub mod play;
+pub mod player;
+pub mod score;
+pub mod token;
+pub mod top_score;
