6b1cd52f31d6b50c4d6cc4e1253785eb16c1dce7
Rafael <rafael_breno@outlook.com>
2022-08-13 21:36:11 -0300

    fix: Update utils to support urls without port

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,6 @@
 pub fn url_build(protocol: String, host: String, port: u16) -> String {
-    format!("{}://{}:{}", protocol, host, port).to_string()
+    match port {
+        0 => format!("{}://{}", protocol, host).to_string(),
+        _ => format!("{}://{}:{}", protocol, host, port).to_string()
+    }
 }
--- tests/utils_test.rs
+++ tests/utils_test.rs
@@ -1,14 +1,26 @@
 #[cfg(test)]
 mod utils_test {
+    use haste::utils::*;
+
     #[test]
     fn test_url_build() {
-        use haste::utils::url_build;
         let actual = url_build(
             "https".to_string(), 
             "localhost".to_string(), 
             8080,
         );
         let expected = "https://localhost:8080".to_string();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_url_build_zero_port() {
+        let actual = url_build(
+            "https".to_string(), 
+            "localhost".to_string(), 
+            0,
+        );
+        let expected = "https://localhost".to_string();
         assert_eq!(actual, expected);
     }
 }
